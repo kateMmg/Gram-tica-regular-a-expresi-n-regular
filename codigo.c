e054e216c72b0341b5222f674277c98cf744bbde
kateMmg <a356943@alumnos.uaslp.mx>
2024-10-21 10:25:49 -0600

    Update codigo.c

--- codigo.c
+++ codigo.c
@@ -1,73 +1,108 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#define TAM 300
 
-typedef struct nodo{
-    char *linea;
-    struct nodo *siguiente;
-}TNodo;
+typedef struct nodo1 
+{
+    char *identificador_regla;
+    char *produccion;              
+    struct nodo1 *sig; 
+} TNodo;
 
-TNodo *crea_nodo(char *linea);
-void inserta_final(TNodo **cab, char *linea);
-TNodo* leerArchivo(TNodo **cab, char *nombreArchivo);
-void imprimirLista(TNodo *cab);
+TNodo *crea_nodo(char *identificador_regla, char *produccion);
+void inserta_final(TNodo **cab, char *identificaodr_regla, char *produccion);
+TNodo* leer_archivo(char *nombre_archivo);
+void imprime(TNodo *cab);
 
-int main() {
-    char *nombreArchivo = "gramatica1.txt";
-    TNodo *cab = NULL;
-    TNodo *lista = leerArchivo(&cab, nombreArchivo);
-    
-    imprimirLista(lista);
+int main() 
+{
+    char *nombre_archivo = "gramatica1.txt";
+    TNodo *lista = leer_archivo(nombre_archivo);
+
+    imprime(lista);
+
+    TNodo *temp;
+    while (lista != NULL) 
+    {
+        temp = lista;
+        lista = lista->sig;
+        free(temp->identificador_regla);
+        free(temp->produccion); 
+        free(temp);    
+    }
     return 0;
 }
 
-TNodo *crea_nodo(char *linea)
+TNodo *crea_nodo(char *identificaodr_regla, char *produccion) 
 {
-    TNodo *nodo;
-    nodo = (TNodo*)malloc(sizeof(TNodo));
-    if (nodo)
+    TNodo *aux = (TNodo *)malloc(sizeof(TNodo));
+    if (aux) 
     {
-        nodo->linea = strdup(linea);
-        nodo->siguiente = NULL;
+        aux->identificador_regla = strdup(identificaodr_regla);
+        aux->produccion = strdup(produccion);
+        aux->sig = NULL;
     }
-    return (nodo);
+    return aux;
 }
 
-void inserta_final(TNodo **cab, char *linea)
+void inserta_final(TNodo **cab, char *identificador_regla, char *produccion) 
 {
-    TNodo *corre;
-    if(*cab == NULL)
-        *cab = crea_nodo(linea);
-    else{
-        corre = *cab;
-        while(corre->siguiente != NULL)
-            corre = corre->siguiente;
-        corre->siguiente = crea_nodo(linea);
+    TNodo *aux = crea_nodo(identificador_regla, produccion);
+    if (aux) 
+    {
+        if (*cab == NULL) 
+        { 
+            *cab = aux;
+        } 
+        else 
+        {
+            TNodo *corre = *cab;
+            while (corre->sig != NULL) 
+            { 
+                corre = corre->sig;
+            }
+            corre->sig = aux;
+        }
     }
 }
 
-TNodo* leerArchivo(TNodo **cab, char *nombreArchivo) {
-    FILE *archivo = fopen(nombreArchivo, "r");
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo");
-        return NULL;
+TNodo* leer_archivo(char *nombre_archivo) 
+{
+    FILE *archivo = fopen(nombre_archivo, "r");
+    if (archivo == NULL) 
+    {
+        perror("Error");
+        exit(EXIT_FAILURE);
     }
+    TNodo *cabeza = NULL;
+    char linea[TAM];
+    char identificador_regla[TAM];
+    char productor[TAM];
 
-    char linea[50];
-    while (fgets(linea, sizeof(linea), archivo)) {
-        linea[strcspn(linea, "\n")] = 0;
-        inserta_final(cab, linea);
+    while (fgets(linea, TAM, archivo)) 
+    {
+        linea[strcspn(linea, "\n")] = '\0';
+        const char *delimitador = strstr(linea, "->");
+        if(delimitador != NULL)
+        {
+            strncpy(identificador_regla, linea, delimitador - linea);
+            identificador_regla[delimitador - linea] = '\0';
+            strcpy(productor, delimitador + 2);
+            inserta_final(&cabeza, identificador_regla, productor); 
+        } 
     }
-
     fclose(archivo);
-    return *cab;
+    return cabeza;
 }
 
-void imprimirLista(TNodo *cab) {
-    TNodo *corre = cab;
-    while (corre != NULL) 
+void imprime(TNodo *cab) 
+{
+    TNodo *aux = cab;
+    printf("Lista impresa en terminal:\n");
+    while (aux != NULL) 
     {
-        printf("%s\n", corre->linea);
-        corre = corre->siguiente;
+        printf("identificador: %s, produccion: %s\n", aux->identificador_regla, aux->produccion);
+        aux = aux->sig;
     }
 }
